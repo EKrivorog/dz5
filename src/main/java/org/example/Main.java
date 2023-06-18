2b95dc43117115d624f7ab18f151f07af59e619f
Evgeniy Krivorog <Evgeniy.krivorog@gmail.com>
2023-06-18 21:46:46 +0300

    dz5 DONE

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,28 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 30;
+        int number4 = 40;
+        int sum1 = number1 + number2;
+        int sum2 = number3 + number4;
+
+        boolean resultFirst = sum1 < sum2;
+        System.out.println(resultFirst);
+
+        sum1 += 1;
+        sum2 -= 2;
+        boolean resultSecond = sum1 > sum2;
+        System.out.println(resultSecond);
+
+        boolean resultThree = sum1 % 2 == 0 || sum2 % 2 == 0;
+        System.out.println(resultThree);
+
+
+
+
     }
 }
